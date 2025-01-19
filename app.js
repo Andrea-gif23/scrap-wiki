93d866bf79842b5e1ab6f70ca8206dc7d717260b
Andrea-gif23 <andreagutierrezromero98@gmail.com>
2025-01-19 14:33:24 +0100

    cambio codigo

--- app.js
+++ app.js
@@ -3,66 +3,28 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 3000;
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.get('/scrape', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-   
     const { data } = await axios.get('https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap');
-
-   
     const $ = cheerio.load(data);
 
- 
-    let resultados = [];
+    const usuarios = [];
 
-   
     $('#mw-pages a').each((index, element) => {
       const enlace = $(element).attr('href');
       const titulo = $(element).text();
-
-    
-      axios.get(`https://es.wikipedia.org${enlace}`).then(response => {
-        const $page = cheerio.load(response.data);
-
-        const tituloPage = $page('h1').text();
-        const imagenes = [];
-        $page('img').each((i, el) => {
-          imagenes.push($page(el).attr('src'));
-        });
-        const textos = [];
-        $page('p').each((i, el) => {
-          textos.push($page(el).text());
-        });
-
-       
-        resultados.push({
-          titulo: tituloPage,
-          imagenes,
-          textos,
-        });
-
-        
-        console.log(`Página: ${tituloPage}`);
-        console.log('Imagenes:', imagenes);
-        console.log('Textos:', textos);
-      }).catch(err => console.log('Error al acceder a la página interna:', err));
+      usuarios.push({ titulo, enlace });
     });
 
-    res.send('Scraping completado, revisa la consola para ver los resultados.');
-
-  } catch (err) {
-    console.error('Error en el scraping:', err);
-    res.status(500).send('Hubo un error al hacer scraping.');
+    res.send(usuarios); // Muestra los datos en el navegador
+  } catch (error) {
+    console.error('Error al hacer scraping:', error);
+    res.status(500).send('Error en el servidor');
   }
 });
 
-
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
